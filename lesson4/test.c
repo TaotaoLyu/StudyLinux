f9c60f22d138984385b73f8aacb4f9b6cc1f3ced
Taotao Lv <leo9627@faxmail.com>
2024-12-19 10:33:50 +0800

    The first C program

---
+++ lesson4/test.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("hello world1\n");
+    printf("hello world2\n");
+    printf("hello world3\n");
+    printf("hello world4\n");
+    printf("hello world5\n");
+    printf("hello world6\n");
+    printf("hello world7\n");
+    printf("hello world8\n");
+    printf("hello world9\n");
+    printf("hello world10\n");
+    printf("hello world\n");
+    return 0;
+}
