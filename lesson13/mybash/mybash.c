50f81bdd96d570e16c59bd8a4e66bdd0b7ea9e3f
Taotao Lyu <taotao.lyu@gmail.com>
2025-02-07 19:49:37 +0800

    enhance myshell

--- lesson13/mybash/mybash.c
+++ lesson13/mybash/mybash.c
@@ -5,14 +5,15 @@
 #include<assert.h>
 #include<string.h>
 #include<unistd.h>
-
+#include<string.h>
 
 
 #define NUM 1024
 #define OPT_NUM 64
 char lineCommand[NUM];
 char* argv_[OPT_NUM];
-
+int lastcode=0;
+int lastsig=0;
 
 
 
@@ -21,7 +22,7 @@ int main()
     while(1)
     {
         printf("[ltt@test_bash lesson13]$ ");
-        char *s=fgets(lineCommand,sizeof(lineCommand),stdin);
+        char *s=fgets(lineCommand,sizeof(lineCommand)-1,stdin);
         assert(s!=NULL);
         (void)s;
         lineCommand[strlen(lineCommand)-1]='\0';
@@ -30,7 +31,29 @@ int main()
         argv_[0]=strtok(lineCommand,(const char*)" ");
         if(argv_[0]==NULL) continue;
         int i=1;
-        while(argv_[i++]=strtok(NULL,(const char*)" ")) ; 
+        if(strcmp(argv_[0],"ls")==0)
+            argv_[i++]=(char*)"--color=auto";
+        while(argv_[i++]=(char*)strtok(NULL,(char*)" ")) ; 
+        
+        if(strcmp(argv_[0],"cd")==0)
+        {
+            if(argv_[1]!=NULL)
+                chdir(argv_[1]);
+            continue;
+        }
+        if(strcmp(argv_[0],"echo")==0)
+        {
+            if(argv_[1]!=NULL&&strcmp(argv_[1],"$?")==0)
+            {
+                printf("exit code: %d, sig num: %d\n",lastcode,lastsig);
+                continue;
+            }   
+        }
+
+
+
+
+
         #ifdef DEBUG
         for(i=0;argv_[i];++i) 
             printf("%s\n",argv_[i]);
@@ -40,16 +63,20 @@ int main()
         if(id==0)
         {
             execvp(argv_[0],argv_); 
-            exit(-1);
+            exit(1);
         }
 
         int status=0;
         pid_t ret=waitpid(id,&status,0);
+        (void)ret;
         assert(ret>0);
         //printf("sig num: %d,exit code: %d \n",status&0x7F,(status>>8)&0xFF);
         //printf("sig num: %d,exit code: %d \n",WIFEXITED(status),WEXITSTATUS(status));
-        
-        if(!WIFEXITED(status)||WEXITSTATUS(status)==-1)
+        lastcode=(status>>8)&0xFF;
+        lastsig=status&0x7F;
+
+
+        if(!WIFEXITED(status)||WEXITSTATUS(status)==1)
             printf("command fail\n");
 
 
